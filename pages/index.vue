71ff31dbc66974f81092bfe0117a604ff0e8dd00
jonathan-martz <github@jmartz.de>
2025-12-01 19:13:18 +0100

    cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -15,44 +15,22 @@
 <script setup lang="ts">
 import ImageSlider from "@/components/content/ImageSlider.vue";
 import ArticelCard from "~/components/blog/ArticelCard.vue";
+import {usePocketBase} from "../utils/pocketbase";
 
-useHead({
+useSeoMeta({
   title: 'Media Store by JMSE - My-Media.world',
-  meta: [
-    {
-      name: 'keywords',
-      content: 'jmse,media store,store,movies,gummersbach,bergneustadt,my,media,world,my-media.world,my-media,my media,filme,serien,kinofilme'
-    },
-    {
-      name: "description",
-      content:
-          "My Media World, ist meine Welt der Medien gefüttert von TMDB mit Film Daten soll es erstmal zeigen was mit PocketStore.io möglich ist by JMSE.",
-    },
-  ],
-  script: [{
-    async: true,
-    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1250525441896834'
-  }]
-});
+  keywords:
+      'jmse,media store,store,movies,gummersbach,bergneustadt,my,media,world,my-media.world,my-media,my media,filme,serien,kinofilme',
+  description:
+      'My Media World, ist meine Welt der Medien gefüttert von TMDB mit Film Daten soll es erstmal zeigen was mit PocketStore.io möglich ist by JMSE.',
+})
 
-const images = [
-  {
-    media: 'https://image.tmdb.org/t/p/original/6U17IFhOo7omnuD2mrFQIaHx82B.jpg'
-  },
-  {
-    media: 'https://image.tmdb.org/t/p/original/ah3m5E28pE4vsFnNvoTTdVxxT3B.jpg'
-  },
-  {
-    media: 'https://image.tmdb.org/t/p/original/ebg8TF2vYaSVNVLv8WO8omebrjw.jpg'
-  }
-]
+const images = ref([]);
+const pb = usePocketBase();
 
 onMounted(() => {
-  nextTick(() => {
-    console.log('tick: ', window.adsbygoogle);
-    if (window.adsbygoogle) {
-      window.adsbygoogle.push({});
-    }
-  })
+  images.value = (await pb.collection('product_pictures').getList(1,10, {
+    filter: 'name="%homepage-%"'
+  })).items;
 });
 </script>
